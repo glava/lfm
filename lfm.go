2fc30271112c1b445028fbb698bd18daf2ef7448
goran <goran.ojkic@gmail.com>
2015-06-13 20:28:43 +0200

    Adding support for listing top tracsk from friends
    Usage:
    	go run lfm.go -u goranche,poohica -p 12month
    
    Goal of this hack was to get familiar with go concurency

--- lfm.go
+++ lfm.go
@@ -1,57 +1,63 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
 	"encoding/json"
 	"flag"
+	"fmt"
+	"github.com/fatih/color"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 type LastFmResponse struct {
 	Toptracks struct {
-				  Track []struct {
-					  Name string `json:"name"`
-					  Duration string `json:"duration"`
-					  Playcount string `json:"playcount"`
-					  Listeners string `json:"listeners"`
-					  Mbid string `json:"mbid"`
-					  URL string `json:"url"`
-					  Streamable struct {
-							   Text string `json:"#text"`
-							   Fulltrack string `json:"fulltrack"`
-						   } `json:"streamable"`
-					  Artist struct {
-							   Name string `json:"name"`
-
-						   } `json:"artist"`
-					  Image []struct {
-						  Text string `json:"#text"`
-						  Size string `json:"size"`
-					  } `json:"image"`
-					  Attr struct {
-							   Rank string `json:"rank"`
-						   } `json:"@attr"`
-				  } `json:"track"`
-				  Attr struct {
-							Artist string `json:"artist"`
-							Page string `json:"page"`
-							Perpage string `json:"perPage"`
-							Totalpages string `json:"totalPages"`
-							Total string `json:"total"`
-						} `json:"@attr"`
-			  } `json:"toptracks"`
+		Track []struct {
+			Name       string `json:"name"`
+			Duration   string `json:"duration"`
+			Playcount  string `json:"playcount"`
+			Listeners  string `json:"listeners"`
+			Mbid       string `json:"mbid"`
+			URL        string `json:"url"`
+			Streamable struct {
+				Text      string `json:"#text"`
+				Fulltrack string `json:"fulltrack"`
+			} `json:"streamable"`
+			Artist struct {
+				Name string `json:"name"`
+			} `json:"artist"`
+			Image []struct {
+				Text string `json:"#text"`
+				Size string `json:"size"`
+			} `json:"image"`
+			Attr struct {
+				Rank string `json:"rank"`
+			} `json:"@attr"`
+		} `json:"track"`
+		Attr struct {
+			Artist     string `json:"artist"`
+			Page       string `json:"page"`
+			Perpage    string `json:"perPage"`
+			Totalpages string `json:"totalPages"`
+			Total      string `json:"total"`
+		} `json:"@attr"`
+	} `json:"toptracks"`
 }
 
 type ApiConfig struct {
 	ApiKey string `json:"api_key"`
 }
 
-
+//TODO: Fold this two functions in one
 func resolveUrl(apiKey string, artist string, limit int) string {
 	return fmt.Sprintf("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=%s&api_key=%s&limit=%d&format=json", artist, apiKey, limit)
 }
 
+func resolveUserUrl(apiKey string, user string, limit int, period string) string {
+	return fmt.Sprintf("http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=%s&api_key=%s&limit=%d&period=%s&format=json", user, apiKey, limit, period)
+}
+
+//TODO: solve this stupid read from home issue. currently it doesnt get the concept of ~
 func readApiConfig() ApiConfig {
 	data, err := ioutil.ReadFile("/Users/goranojkic/.lfm/config")
 	var auth ApiConfig
@@ -84,37 +90,69 @@ func parseJson(jsonBody []byte, result interface{}) error {
 	return json.Unmarshal(jsonBody, &result)
 }
 
+//TODO: fold to one function artistSongs and userSongs
 func artistSongs(apiKey string, artist string, limit int) {
 	var url = resolveUrl(apiKey, artist, limit)
 	body, error := executeRequest(url)
 
-	var reponse LastFmResponse
-	parseJson(body, &reponse)
+	var response LastFmResponse
+	parseJson(body, &response)
 
 	if error != nil {
 		fmt.Printf("%s", error)
 	} else {
-		fmt.Println(reponse.Toptracks.Track[1].Name)
+
+		for _, v := range response.Toptracks.Track {
+			fmt.Println(artist + "-" + v.Name)
+		}
 	}
 }
 
+func userSongs(apiKey string, user string, limit int, period string) chan LastFmResponse {
+	yield := make(chan LastFmResponse)
+	go func() {
+		var url = resolveUserUrl(apiKey, user, limit, period)
+		body, error := executeRequest(url)
+
+		var response LastFmResponse
+		parseJson(body, &response)
+
+		if error != nil {
+			fmt.Printf("%s", error)
+		} else {
+
+			yield <- response
+		}
+	}()
+
+	return yield
+}
+
+func outputTracks(response LastFmResponse, optionalTitle string) {
+	if optionalTitle != "" {
+		color.Green(optionalTitle)
+	}
+	for _, v := range response.Toptracks.Track {
+		color.Yellow("\t" + v.Artist.Name + " - " + v.Name)
+	}
+}
 
 func main() {
 	apiConfig := readApiConfig()
-
 	artistName := flag.String("a", "", "artist name")
 	limit := flag.Int("l", 20, "limit")
 
-	//userNames := flag.String("u", "", "last.fm user names comma separated")
-	//range := flag.String("r", "", "time range you are searching for")
+	userNames := flag.String("u", "", "last.fm user names comma separated")
+	period := flag.String("p", "", "period you are searching for")
 
 	flag.Parse()
 
 	if *artistName != "" {
 		artistSongs(apiConfig.ApiKey, *artistName, *limit)
 	} else {
-		fmt.Println("Not yet implemented")		
+		for _, user := range strings.Split(*userNames, ",") {
+			outputTracks(<-userSongs(apiConfig.ApiKey, user, *limit, *period), user)
+		}
 	}
 
 }
-
