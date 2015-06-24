c75be371b1e8ca98df36df1dd6c21b5b496845a6
goran <goran.ojkic@gmail.com>
2015-06-24 16:06:34 +0200

    saving token and session to a file
    - needs revision

--- config/config.go
+++ config/config.go
@@ -12,6 +12,12 @@ type ApiConfig struct {
 	ApiSecret string `json:"api_secret"`
 }
 
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
 func parse(jsonBody []byte, result interface{}) error {
 	return json.Unmarshal(jsonBody, &result)
 }
@@ -26,4 +32,28 @@ func Load() ApiConfig {
 
 	parse(data, &auth)
 	return auth
+}
+
+func SaveToken(token string) {
+ 	d1 := []byte(token)
+    err := ioutil.WriteFile(os.Getenv("HOME") + "/.lfm/token", d1, 0644)
+    check(err)
+}
+
+func GetToken() string {
+	dat, err := ioutil.ReadFile(os.Getenv("HOME") + "/.lfm/token")
+    check(err)
+    return string(dat)
+}
+
+func SaveSession(session string) {
+ 	d1 := []byte(session)
+    err := ioutil.WriteFile(os.Getenv("HOME") + "/.lfm/session", d1, 0644)
+    check(err)
+}
+
+func GetSession() string {
+	dat, err := ioutil.ReadFile(os.Getenv("HOME") + "/.lfm/session")
+    check(err)
+    return string(dat)
 }
